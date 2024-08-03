c99698259df386c8d11c7c7d66a1dbb7ad91e6d8
LukaSukhitashvili <suxxosuxxo99@gmail.com>
2024-08-04 00:41:05 +0400

    changes in HW

--- Level 19/homework/username.py
+++ Level 19/homework/username.py
@@ -1,7 +1,8 @@
 user_name = input("Enter user's name: ")
-your_name = input("Enter your name: ")
+my_name = "luka"
+my_name_2 = "Luka"
 
-while user_name != your_name:
+while user_name != my_name and user_name != my_name_2:
     print("Wrong user name! Try again!")
     user_name = input("Enter user's name: ")
     your_name = input("Enter your name: ")
