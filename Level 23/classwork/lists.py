77525341e24f99dd8694d77d752fe687f1ca46a7
LukaSukhitashvili <suxxosuxxo99@gmail.com>
2024-08-12 22:34:47 +0400

    added level 23 classwork

---
+++ Level 23/classwork/lists.py
@@ -0,0 +1,7 @@
+fruits = ["apple", "banana", "cherry", "orange", "kiwi",]
+
+fruits[0] = "melon"
+fruits[1] = "pear"
+fruits[5] = "grape"
+
+print(fruits)
---
+++ Level 23/classwork/words_list.py
@@ -0,0 +1,3 @@
+sentence = ["Goa", "Is", "Best", "Academy", "In", "The World"]
+
+print(sentence[0] + " " + sentence[1] + " " + sentence[2] + " " + sentence[3] + " " + sentence[4] + " " + sentence[5])
