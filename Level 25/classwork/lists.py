ae1ab88413461946746a86bc603562cee0010379
LukaSukhitashvili <suxxosuxxo99@gmail.com>
2024-08-17 13:43:35 +0400

    Added level 25 HW

---
+++ Level 25/classwork/lists.py
@@ -0,0 +1,18 @@
+# lists = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+# print(len(lists))
+
+# print(lists[0:10])
+
+# print(lists[0:9:2])
+
+# print(lists[0::2])
+
+# print(lists[:9:2])
+
+# print(lists[::-1])
+
+
+lists = [1, 2, 3, 4, 5]
+print(lists[1:-2])
+
---
+++ Level 25/homework/gameTo_progLanguage.py
@@ -0,0 +1,9 @@
+games = ["Minecraft", "Valorant", "Fortnite", "Roblox", "GTA San Andreas"]
+
+games[0] = "Python"
+games[1] = "Javascript"
+games[2] = "C#"
+games[3] = "C++"
+games[4] = "Java"
+
+print(games)
