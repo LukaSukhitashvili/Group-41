26b1f09d14c3cb5e288e61b8883f6162942c3aab
LukaSukhitashvili <suxxosuxxo99@gmail.com>
2024-08-24 20:56:34 +0400

    added level 28 hw

--- Level 26/homework/bmi_calc.py
+++ Level 26/homework/bmi_calc.py
@@ -1,13 +1,13 @@
 weight = float(input("Enter your weight in kg: "))
 height = float(input("Enter your height in m: "))
 
-total = weight / (height ** 2)
+BMI = weight / (height ** 2)
 
-if total < 18.5:
+if BMI < 18.5:
     print("You're: Underweight")
-elif total >= 18.5 or 25.0:
+elif BMI >= 18.5 or 25.0:
     print("You're: Normal")
-elif total >= 25.0 or 30.0:
+elif BMI >= 25.0 or 30.0:
     print("You're: Overweight")
-elif total >= 30.0:
+elif BMI >= 30.0:
     print("You're: Obesity")
