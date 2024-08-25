48cec9989d461ac7c8855442c5396ff034099d79
LukaSukhitashvili <suxxosuxxo99@gmail.com>
2024-08-25 12:53:22 +0400

    corrected bmi Calculator

--- Level 26/homework/bmi_calc.py
+++ Level 26/homework/bmi_calc.py
@@ -5,9 +5,9 @@ BMI = weight / (height ** 2)
 
 if BMI < 18.5:
     print("You're: Underweight")
-elif BMI >= 18.5 or 25.0:
+elif BMI >= 18.5 and BMI < 25.0:
     print("You're: Normal")
-elif BMI >= 25.0 or 30.0:
+elif BMI >= 25.0 and BMI < 30.0:
     print("You're: Overweight")
-elif BMI >= 30.0:
+else:
     print("You're: Obesity")
