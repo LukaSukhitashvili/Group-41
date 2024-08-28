0fcefcb0202c455b0079054406709e5b2e48120e
LukaSukhitashvili <suxxosuxxo99@gmail.com>
2024-08-28 12:51:35 +0400

    Added level 29, 30, and 31 HW

---
+++ Level 31/classwork/list.py
@@ -0,0 +1,5 @@
+my_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+
+print(my_list[3:13])
+print(my_list[:11])
+print(my_list[::4])
---
+++ Level 31/homework/fruits.py
@@ -0,0 +1,10 @@
+fruits = ['Apple', 'banana', 'cherry', 'date', 'fig', 'grape']
+
+print(fruits[::-1])
+
+print("---------------------------------------")
+
+
+fruits[2] = "Kiwi"
+fruits[3] = "Lime"
+print(fruits)
