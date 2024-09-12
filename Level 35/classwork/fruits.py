e9f540c5f07791c7edbc43e3bf97e18da2f9dcc5
LukaSukhitashvili <suxxosuxxo99@gmail.com>
2024-09-12 21:51:26 +0400

    Added level 35 CW

---
+++ Level 35/classwork/fruits.py
@@ -0,0 +1,15 @@
+fruit = ["apple", "banana", "cherry", "date", "elderberry"]
+
+print(fruit)
+
+print(fruit[0])
+print(fruit[-1])
+
+fruit.append("fig")
+fruit.remove("banana")
+
+fruit.insert(1, "blueberry")
+
+print(fruit)
+
+print(len(fruit))
---
+++ Level 35/classwork/list_functions_and_methods.py
@@ -0,0 +1,19 @@
+numbers = [10, 20, 30, 40, 50, 60, 70, 80, 90]
+
+numbers.append(100)
+
+numbers.insert(0, 5)
+
+numbers.remove(30)
+
+numbers.sort()
+
+numbers.reverse()
+
+index_of_50 = numbers.index(50)
+print(f"The index of 50 is: {index_of_50}")
+
+numbers.count(20)
+
+print(numbers)
+
